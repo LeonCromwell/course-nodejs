adb84fb9f5e874d66023d483bcdf69dbf85dd05c
LeonCromwell <mailyhai814@gmail.com>
2023-08-25 16:30:01 +0700

    template engine (handlebars)

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import { engine } from "express-handlebars";
+import morgan from "morgan"
+import express from "express"
+import path from "path"
+import { fileURLToPath } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const port = 3000;
+
+
+//http logger
+app.use(morgan("combined"));
+
+// template engine
+app.engine("hbs", engine({
+  extname: "hbs"
+}));
+app.set("view engine", "hbs");
+app.set("views",  path.join(__dirname, "resources/views"));
+
+// console.log("path: " , path.join(__dirname, "resources/views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/news", (req, res) => {
+  res.render("news");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
