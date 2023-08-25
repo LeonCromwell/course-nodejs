5946f84c70b66a9cc4b6dcbb7a4201eba2c95ef3
LeonCromwell <mailyhai814@gmail.com>
2023-08-25 17:07:54 +0700

    cấu hình sass, scss

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, "public")))
 
 //http logger
 app.use(morgan("combined"));
