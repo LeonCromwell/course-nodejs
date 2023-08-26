2a9de67f9b698ff50849afd87ab36f6db98e1d69
LeonCromwell <mailyhai814@gmail.com>
2023-08-26 22:03:41 +0700

    Post method turtorial

--- src/index.js
+++ src/index.js
@@ -13,6 +13,10 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, "public")))
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(express.json())
 
 //http logger
 app.use(morgan("combined"));
@@ -24,8 +28,8 @@ app.engine("hbs", engine({
 app.set("view engine", "hbs");
 app.set("views",  path.join(__dirname, "resources/views"));
 
-// console.log("path: " , path.join(__dirname, "resources/views"));
 
+//basic routing
 app.get("/", (req, res) => {
   res.render("home");
 });
@@ -33,6 +37,16 @@ app.get("/", (req, res) => {
 app.get("/news", (req, res) => {
   res.render("news");
 });
+
+app.get("/search", (req, res) => {
+  res.render("search");
+});
+app.post("/search", (req, res) => {
+  console.log(req.body);
+ res.send("")
+});
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
