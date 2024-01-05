e3e6432e9402ac157741488efc85adc200978d19
LeonCromwell <mailyhai814@gmail.com>
2024-01-05 18:24:31 +0700

    test lòi mắt

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -4,7 +4,7 @@ export async function connect() {
     try {
         mongoose.set('strictQuery', false);
         await mongoose.connect(
-            'mongodb+srv://mailyhai814:992003%40@cluster1.rh5edte.mongodb.net/?retryWrites=true&w=majority',
+            'mongodb+srv://mailyhai814:992003Hai@cluster1.rh5edte.mongodb.net/?retryWrites=true&w=majority',
             {
                 useNewUrlParser: true,
                 useUnifiedTopology: true,
