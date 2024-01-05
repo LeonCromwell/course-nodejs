1cf7f52b16602d62cbc2fc98b5b643c4a019feca
LeonCromwell <mailyhai814@gmail.com>
2024-01-05 17:47:07 +0700

    looxiii lòi

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 export async function connect() {
     try {
+        mongoose.set('strictQuery', false);
         await mongoose.connect('mongodb+srv://mailyhai814:992003Hai%40@cluster1.rh5edte.mongodb.net/F8_Education_dev', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
