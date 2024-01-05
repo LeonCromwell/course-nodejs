b93f52978254e9a30f5b04387f4a86d5f16e3ace
LeonCromwell <mailyhai814@gmail.com>
2024-01-05 17:55:53 +0700

    testt lần thứ n+1

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -3,10 +3,13 @@ import mongoose from 'mongoose';
 export async function connect() {
     try {
         mongoose.set('strictQuery', false);
-        await mongoose.connect('mongodb+srv://mailyhai814:992003Hai%40@cluster1.rh5edte.mongodb.net/F8_Education_dev', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(
+            'mongodb+srv://mailyhai814:992003%40@cluster1.rh5edte.mongodb.net/?retryWrites=true&w=majority',
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            },
+        );
         console.log('Connected!');
     } catch (error) {
         console.log(error);
