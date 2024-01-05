10968701953f7cd6c9969c967bc611e106bb6334
LeonCromwell <mailyhai814@gmail.com>
2024-01-05 17:35:54 +0700

    testtt

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,18 +1,13 @@
 import mongoose from 'mongoose';
 
-
-export async function  connect() {
-   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/F8_Education_dev', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-    console.log("Connected!");
-
-
-   } catch (error) {
-    console.log(error);
-   }
+export async function connect() {
+    try {
+        await mongoose.connect('mongodb+srv://mailyhai814:992003Hai@@cluster1.rh5edte.mongodb.net/F8_Education_dev', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Connected!');
+    } catch (error) {
+        console.log(error);
+    }
 }
-
-
